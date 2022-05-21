8bc403cbd3e5e97c4da0dfc68feaf988718a7eda
박세현 <king1209sam@naver.com>
2022-05-21 19:15:27 +0900

    add register from ver1.0

--- src/components/layout/HeaderNaviBar.vue
+++ src/components/layout/HeaderNaviBar.vue
@@ -28,12 +28,6 @@
             ></b-nav-item
           >
           <b-nav-item href="#"
-            ><router-link :to="{ name: 'instagram' }" class="link"
-              ><b-icon icon="instagram" font-scale="1.5"></b-icon>
-              인별그램</router-link
-            ></b-nav-item
-          >
-          <b-nav-item href="#"
             ><router-link :to="{ name: 'house' }" class="link"
               ><b-icon icon="house-fill" font-scale="1.5"></b-icon>
               아파트정보</router-link
--- src/components/user/MemberRegister.vue
+++ src/components/user/MemberRegister.vue
@@ -5,12 +5,91 @@
         <b-alert variant="secondary" show><h3>회원가입</h3></b-alert>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col></b-col>
+      <b-col cols="8">
+        <b-card class="text-center mt-3" style="max-width: 40rem" align="left">
+          <b-form class="text-left">
+            <b-form-group label="아이디:" label-for="userid">
+              <b-form-input
+                id="userid"
+                v-model="user.userid"
+                required
+                placeholder="아이디 입력...."
+                @keyup.enter="confirm"
+              ></b-form-input>
+              <b-button
+                type="button"
+                variant="primary"
+                class="m-1"
+                @click="넣어야함;"
+                >아이디 중복 확인</b-button
+              >
+            </b-form-group>
+            <b-alert show variant="danger" v-if="idDuplicate"
+              >사용할 수 없는 아이디 입니다.</b-alert
+            >
+            <b-form-group label="비밀번호:" label-for="userpwd">
+              <b-form-input
+                type="password"
+                id="userpwd"
+                v-model="user.userpwd"
+                required
+                placeholder="비밀번호 입력...."
+                @keyup.enter="넣어야함;"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="이름:" label-for="username">
+              <b-form-input
+                id="username"
+                v-model="user.username"
+                required
+                placeholder="이름 입력...."
+                @keyup.enter="넣어야함;"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="이메일:" label-for="email">
+              <b-form-input
+                type="email"
+                id="email"
+                v-model="user.email"
+                required
+                placeholder="이메일 입력...."
+                @keyup.enter="넣어야함;"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button
+              type="button"
+              variant="success"
+              class="m-1"
+              @click="movePage"
+              >회원가입</b-button
+            >
+          </b-form>
+        </b-card>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 export default {
   name: "MemberRegister",
+  data() {
+    return {
+      user: {
+        userid: null,
+        userpwd: null,
+        username: null,
+        email: null,
+        joindate: null,
+      },
+      idDuplicate: true,
+      allCheck: false,
+    };
+  },
 };
 </script>
 
